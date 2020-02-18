e5ffeb86b0ab8f6ffa0209a2c994bc342a92e0db
Dayanand <dayanand@kribhcoindia.com>
2020-02-18 11:41:43 +0530

    update invoice entity and provider entity

--- modules/custom/e_invoice_cr/modules/invoice_entity/src/Controller/InvoiceEntityController.php
+++ modules/custom/e_invoice_cr/modules/invoice_entity/src/Controller/InvoiceEntityController.php
@@ -188,7 +188,6 @@ class InvoiceEntityController extends ControllerBase implements ContainerInjecti
 
     /** @var \Drupal\invoice_entity\InvoiceService $invoice_service */
     $invoice_service = \Drupal::service('invoice_entity.service');
-    $invoice_service->setConsecutiveNumber($type_of);
     $result = $invoice_service->validateInvoiceEntity($entity);
 
     // Verify the result of the invoice validation.
@@ -226,22 +225,13 @@ class InvoiceEntityController extends ControllerBase implements ContainerInjecti
 
     // Gets the documents of the invoice.
     $pdf_file = File::load($this->searchFile('invoice_' . $id . '.pdf'));
-    $signed_file = File::load($this->searchFile('document-' . $user_id . '-' . $consecutive . 'segned.xml'));
-    $confirmation_file = File::load($this->searchFile('document-' . $user_id . '-' . $consecutive . 'confirmation.xml'));
+    
 
     // Create a new zip file and save it in a temporary directory.
-    $zip = new \ZipArchive();
-    $uri = file_directory_temp() . '/invoice' . $id . '.zip';
-    $zip->open($uri, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
+    
 
     // Attach files in the zip.
-    $zip->addFile(\Drupal::service('file_system')->realpath($pdf_file->getFileUri()),
-      $pdf_file->getFilename());
-    $zip->addFile(\Drupal::service('file_system')->realpath($signed_file->getFileUri()),
-      $signed_file->getFilename());
-    $zip->addFile(\Drupal::service('file_system')->realpath($confirmation_file->getFileUri()),
-      $confirmation_file->getFilename());
-    $zip->close();
+    
 
     // Downloads automatically the zip file in the device.
     header('Content-type: application/octet-stream');
