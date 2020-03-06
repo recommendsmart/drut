7b73d548d5bd7a2d98fd727c0c1601b76711358d
Dayanand <dayanand@kribhcoindia.com>
2020-03-06 13:47:14 +0530

    revert cr settings

--- modules/custom/e_invoice_cr/src/Form/InvoiceSettingsForm.php
+++ modules/custom/e_invoice_cr/src/Form/InvoiceSettingsForm.php
@@ -31,12 +31,15 @@ class InvoiceSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $base_url = $host = \Drupal::request()->getHost();
     $options_env = [
       "1" => $this->t("Production"),
+      "2" => $this->t("Sandbox"),
     ];
     $settings = \Drupal::config('e_invoice_cr.settings');
     // Get default values.
     $environment = $settings->get('environment');
+    $logo_file = $settings->get('invoice_logo_file');
 
     $form['environment'] = [
       '#type' => 'select',
@@ -44,8 +47,33 @@ class InvoiceSettingsForm extends ConfigFormBase {
       '#default_value' => $environment,
       '#required' => TRUE,
       '#options' => $options_env,
-      '#description' => $this->t('Select "Production" to set the production mode.'),
+      '#description' => $this->t('Select "Production" to set the production mode or "Sandbox" to set the tests mode.'),
       '#validated' => TRUE,
+    ];
+    $form['settings_tab'] = [
+      '#type' => 'horizontal_tabs',
+      '#tree' => TRUE,
+      '#prefix' => '<div id="settings-invoice-wrapper">',
+      '#suffix' => '</div>',
+    ];
+    $form['settings_tab']['stuff']['email_text_group'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Email notifications.'),
+      '#collapsed' => FALSE,
+    ];
+    $form['settings_tab']['stuff']['email_text_group']['invoice_logo_file'] = [
+      '#title' => $this->t('Company Logo'),
+      '#type' => 'managed_file',
+      '#description' => $this->t('Add a company logo that it will be print on the invoice documents.'),
+      '#upload_validators' => [
+        'file_validate_extensions' => ['png jpg jpeg'],
+        'file_validate_image_resolution' => ["300x300", ""],
+      ],
+      '#default_value' => $logo_file,
+      '#theme' => 'image_widget',
+      '#preview_image_style' => 'medium',
+      '#upload_location' => 'public://',
+      '#required' => FALSE,
     ];
 
     return parent::buildForm($form, $form_state);
@@ -62,7 +90,8 @@ class InvoiceSettingsForm extends ConfigFormBase {
     \Drupal::configFactory()->getEditable('e_invoice_cr.settings')
       // Set the submitted configuration setting.
       ->set('environment', $form_state->getValue('environment'))
-
+      ->set('invoice_logo_file', $tabs['email_text_group']['invoice_logo_file'])
+      ->save('file', $tabs['email_text_group']['invoice_logo_file']);
     parent::submitForm($form, $form_state);
   } 
 }
