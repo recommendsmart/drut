07f5fddbd05054cfa2bc2bbe0db1038ccc764eb5
Dayanand <dayanand@kribhcoindia.com>
2020-03-06 15:33:45 +0530

    remove invoiceNumber;

--- modules/custom/e_invoice_cr/modules/invoice_entity/src/InvoiceService.php
+++ modules/custom/e_invoice_cr/modules/invoice_entity/src/InvoiceService.php
@@ -10,57 +10,8 @@ use Drupal\invoice_entity\Entity\InvoiceEntityInterface;
  */
 class InvoiceService implements InvoiceServiceInterface {
 
-  protected static $invoiceNumber;
-  protected static $consecutiveName;
-
-  /**
-   * Update the configuration values.
-   */
-
   /**
    * {@inheritdoc}
    */
-  public function setConsecutiveNumber($documentType) {
-
-    switch ($documentType) {
-      case 'FE':
-        self::$consecutiveName = 'electronic_bill_consecutive';
-        break;
-
-      case 'ND':
-        self::$consecutiveName = 'debit_note_consecutive';
-        break;
-
-      case 'NC':
-        self::$consecutiveName = 'credit_note_consecutive';
-        break;
-
-      case 'TE':
-        self::$consecutiveName = 'electronic_ticket_consecutive';
-        break;
-
-      case '1':
-        self::$consecutiveName = 'invoice_accepted_consecutive';
-        break;
-
-      case '2':
-        self::$consecutiveName = 'invoice_partial_accepted_consecutive';
-        break;
-
-      case '3':
-        self::$consecutiveName = 'invoice_rejected_consecutive';
-        break;
-
-      default:
-        self::$consecutiveName = 'electronic_bill_consecutive';
-        break;
-    }
-
-    self::$invoiceNumber = $this->getInvoiceVariable(self::$consecutiveName);
-    if (is_null(self::$invoiceNumber)) {
-      self::$invoiceNumber = '0000000001';
-      $this->updateValues();
-    }
-  }
 
 }
