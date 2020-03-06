2599e9a4f5dacd31fadb86dcb9fc28586d6d6a1c
Dayanand <dayanand@kribhcoindia.com>
2020-03-06 17:13:08 +0530

    revert invoice service

---
+++ modules/custom/e_invoice_cr/modules/invoice_entity/src/InvoiceService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\invoice_entity;
+
+use Drupal\invoice_entity\Entity\InvoiceEntity;
+use Drupal\invoice_entity\Entity\InvoiceEntityInterface;
+
+/**
+ * Class InvoiceService.
+ */
+class InvoiceService implements InvoiceServiceInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+
+}
